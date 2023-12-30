6cc32a027f9c547af168571a5ac0ebe71e2b0c90
ktro2828 <kotaro.uetake@tier4.jp>
2023-12-30 19:18:39 +0900

    docs: update the documents for camera
    Signed-off-by: ktro2828 <kotaro.uetake@tier4.jp>

--- src/camera.rs
+++ src/camera.rs
@@ -6,10 +6,12 @@ use crate::ray::Ray;
 /// A struct to represent camera.
 ///
 /// # Arguments
-/// * `origin`  - An origin position of camera, a.k.a lookfrom.
-/// * `u`
-/// * `v`
-/// * `w`
+/// * `origin`  - The origin position of camera, a.k.a lookfrom.
+/// * `u`       - The x direction base vector.
+/// * `v`       - The y direction base vector.
+/// * `w`       - The z direction base vector.
+/// * `width`   - The image width.
+/// * `height`  - The image height.
 #[derive(Debug)]
 pub struct Camera {
     pub origin: Vec3,
@@ -20,13 +22,25 @@ pub struct Camera {
     pub height: u32,
 }
 
+#[cfg_attr(doc, katexit::katexit)]
 impl Camera {
-    /// Constructs `Camera` instance from uvw vectors.
+    /// Constructs `Camera` instance from the base vectors $\vec{u}$, $\vec{v}$, $\vec{w}$.    
+    ///
+    /// $$
+    /// u = \frac{i}{W}, \quad v = \frac{j}{H} \quad (0\leq u,v \leq1 )
+    /// $$
+    ///
+    /// with the base vectors $\vec{u}$, $\vec{v}$, $\vec{w}$,
+    /// the ray direction $\vec{p}$ is formulated as follows
+    ///
+    /// $$
+    /// \vec{p} = \vec{u} \cdot u + \vec{v} \cdot v + \vec{w}
+    /// $$
     ///
     /// # Arguments
-    /// * `u`       -
-    /// * `v`       -
-    /// * `w`       -
+    /// * `u`       - The x direction base vector $\vec{u}$.
+    /// * `v`       - The y direction base vector $\vec{v}$.
+    /// * `w`       - The z direction base vector $\vec{w}$.
     /// * `width`   - The image width.
     /// * `height`  - The image height.
     ///
@@ -103,6 +117,16 @@ impl Camera {
         }
     }
 
+    /// Returns the ray with the normalized pixel positions `u`, `v`.
+    /// The ray direction $\vec{p}$ is as follows.
+    ///
+    /// $$
+    /// \vec{p} = \vec{u} \cdot u + \vec{v} \cdot v + \vec{w}
+    /// $$
+    ///
+    /// # Arguments
+    /// * `u`   - The x direction normalized pixel position.
+    /// * `v`   - The y direction normalized pixel position.
     pub fn ray(&self, u: f64, v: f64) -> Ray {
         Ray {
             origin: self.origin,
